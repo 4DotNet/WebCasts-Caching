9fd34c4c714a4fd86a148f662eeca76518f988ce
Eduard Keilholz <e.keilholz@4dotnet.nl>
2020-07-23 21:10:08 +0200

    Add in-mem example

---
+++ src/4DNCaching.InMemory.Api/Controllers/DownloadController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace _4DNCaching.InMemory.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+
+
+        public async Task<IActionResult> Get()
+        {
+            // Get from cache
+            // If not in cache -> Download stuff
+            // Add cache
+            // Response
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+
+        public DownloadController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+    }
+}
