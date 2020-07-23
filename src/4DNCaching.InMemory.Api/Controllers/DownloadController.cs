c0773be6acc7cbecfbac8b966b888fdfc797613f
Eduard Keilholz <e.keilholz@4dotnet.nl>
2020-07-23 21:26:58 +0200

    Added mem caching implementation

--- src/4DNCaching.InMemory.Api/Controllers/DownloadController.cs
+++ src/4DNCaching.InMemory.Api/Controllers/DownloadController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -13,12 +14,20 @@ namespace _4DNCaching.InMemory.Api.Controllers
 
         public async Task<IActionResult> Get()
         {
-            // Get from cache
-            // If not in cache -> Download stuff
-            // Add cache
-            // Response
-            await Task.CompletedTask;
-            return Ok();
+            var cacheKey = "downloaded-gist-response";
+            var responseData = _cache.Get<string>(cacheKey);
+            if (string.IsNullOrEmpty(responseData))
+            {
+                var httpClient = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                    "https://gist.githubusercontent.com/nikneem/66060dad016cabd426166f282a946f80/raw/581fe051d1b84c6799eecbdca5496b69c474292c/4DotNet-WebCasts-Caching.json");
+
+                var response = await httpClient.SendAsync(request);
+                 responseData = await response.Content.ReadAsStringAsync();
+
+                _cache.Set(cacheKey, responseData);
+            }
+            return Ok(responseData);
         }
 
 
